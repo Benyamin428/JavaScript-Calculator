f840432022c89302f958db7d4ceb6522f21c568b
BenyaminB <60136796+BenyaminB@users.noreply.github.com>
2022-06-30 17:35:38 +0100

    Addition and Subtraction JS code

---
+++ main.js
@@ -0,0 +1,34 @@
+let value = "";
+let displayValue = "";
+
+const button = (item) => {
+    if (item == "+" || item == "-" || item == "=") {
+        equal();
+    }
+    value+=item;
+    displayValue+=item;
+
+    if (/^[0-9]$/.test(value[value.length-1])) {
+        document.getElementById("displayScreen").innerHTML = displayValue;
+    }
+    else {
+        displayValue = "";
+    }
+    console.log(value);
+}
+
+const equal = () => {
+    if (value.includes("+")) {
+        const expression = value.split("+");
+        value = parseFloat(expression[0]) + parseFloat(expression[1]);
+    }
+    else if (value.includes("-")) {
+        const expression = value.split("-");
+        value = parseFloat(expression[0]) - parseFloat(expression[1]);
+    }
+    else if (value.includes("=")) {
+        const expression = value.split("=");
+        value = parseFloat(expression[0]);
+    }
+    document.getElementById("displayScreen").innerHTML = value;
+}
